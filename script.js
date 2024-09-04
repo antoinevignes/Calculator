36a4b22031f0a9ac9321e40a9ead99a0bf62d12a
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-09-04 22:55:17 +0200

    Small changes

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
+// On initialise des variables dans lesquelles on va stocker les chiffres
+
 let firstOperand = "";
 let secondOperand = "";
 let currentOperation = null;
+let shouldResetScreen = false;
 
-//Get the differents components
+// On crée les fonctions avec les operateurs
 
-const screen = document.querySelector("#screen");
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Fonction qui permet d'utiliser les opérations
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return substract(a, b);
+      break;
+    case "*":
+      return divide(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      return null;
+  }
+}
+
+// On vient récupérer les différents boutons
+
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const clearButton = document.querySelector("#clear-button");
 const equalButton = document.querySelector("#equal-button");
 const dotButton = document.querySelector("#dot-button");
+const screen = document.querySelector("#screen");
 
-// Event listeners for the buttons
+// On fait les events listeners pour les boutons
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent));
@@ -22,86 +63,48 @@ operatorButtons.forEach((button) => {
 });
 
 clearButton.addEventListener("click", clear);
-
 equalButton.addEventListener("click", evaluate);
-
 dotButton.addEventListener("click", appendDot);
 
-// Calculator functions
-
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-
-const substract = (num1, num2) => {
-  return num1 - num2;
-};
-
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
-
-const divide = (num1, num2) => {
-  return num1 / num2;
-};
-
-// Get the result function
-
-function operate(num1, num2, operator) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2);
-      break;
-    case "-":
-      return substract(num1, num2);
-      break;
-    case "*":
-      return multiply(num1, num2);
-      break;
-    case "/":
-      return divide(num1, num2);
-      break;
-    default:
-      return null;
-  }
-}
-
-//Reset the screen content (HELPER FUNCTION)
-
-function resetScreen() {
-  screen.textContent = "";
-}
-
-// Clear the screen (HELPER FUNCTION)
-
-function clear() {
-  screen.textContent = "";
-}
-
-//Add the number to the screen
+// Fonctions helper
 
 function appendNumber(number) {
+  if (screen.textContent === "0" || shouldResetScreen) resetScreen();
   screen.textContent += number;
 }
 
-// Append dot function
-
 function appendDot() {
+  if (shouldResetScreen) resetScreen();
+  if (screen.textContent.includes(".")) return;
   screen.textContent += ".";
 }
 
-// Store the firstOperand variant and the operator variant
-
 function setOperation(operator) {
+  if (currentOperation !== null || shouldResetScreen) evaluate();
   firstOperand = parseFloat(screen.textContent);
   currentOperation = operator;
-  screen.textContent = operator;
-  console.log(typeof firstOperand);
+  screen.textContent = currentOperation;
 }
 
-// Store the second operand, get the result and show it on screen
-
 function evaluate() {
+  if (currentOperation === null) return;
+  if (currentOperation === "/" && screen.textContent === "0") {
+    alert("You cannot divide by 0");
+    return;
+  }
   secondOperand = parseFloat(screen.textContent);
   screen.textContent = operate(firstOperand, secondOperand, currentOperation);
+  currentOperation = null;
+}
+
+function clear() {
+  screen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
 }
